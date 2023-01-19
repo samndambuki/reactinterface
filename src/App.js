54390b50255f238d40cabea1adc574005d3d5033
samndambuki <sam.ndambuki08@gmail.com>
2023-01-19 23:24:29 +0300

    first component unfinished

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App container mx-auto mt-3 font-thin">
       <h1 className="text-5xl">
-        <BiArchive className="inline-block text-red-400 align-top" />Your Appointments</h1>
+        <BiArchive className="inline-block text-red-400 align-top"/>Your Appointments</h1>
     </div>
   );
 }
